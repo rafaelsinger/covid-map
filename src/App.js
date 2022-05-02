1e05fb012a3368c4d7117e0fe423d6e9c23d7031
rafaelsinger <rafaelsinger66@gmail.com>
2022-05-02 00:40:40 -0400

    fixed fetchCoords and reintroduced the map

--- src/App.js
+++ src/App.js
@@ -1,25 +1,112 @@
-import logo from './logo.svg';
-import './App.css';
+/*
+TODO: map through fetchCovidData and display it on the page
+TODO: figure out where/how to apply covid data to the map
+TODO: make custom markers and display further data to add interactivity
+TODO: allow for more functionality and expand further
+TODO: make everything look better/unique through custom CSS
+*/
 
-function App() {
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import { OpenStreetMapProvider } from 'leaflet-geosearch';
+import { Icon } from 'leaflet';
+import states from './states.json';
+
+const state = new Icon({
+  iconUrl: './clipboard.svg',
+  iconSize: [25, 25]
+})
+
+
+const provider = new OpenStreetMapProvider();
+
+export default function App() {
+
+  const [covidData, setCovidData] = useState({});
+  const [stateNames, setStateNames] = useState([]);
+  const [coords, setCoords] = useState([]);
+  const [activeState, setActiveState] = useState(null);
+
+  useEffect(() => {
+    if (!stateNames || !stateNames.length){
+      return;
+    }
+    fetchCoords();
+  }, [stateNames])
+
+  useEffect(() => {
+    getStateNames();
+  }, [])
+
+  // useEffect(() => {
+  //   console.log(activeState);
+  //   // fetchCovidData(activeState);
+  // }, [activeState]);
+
+  const fetchCoords = async () => {
+    try {
+      const req = await Promise.all(stateNames.map(async (state) => {
+        return await axios.get(`https://nominatim.geocoding.ai/search.php?state=${state}&format=jsonv2`);
+      }))
+      for (let i = 0; i < req.length; i++){
+        const stateInfo = req[i].data[0];
+        if (req[i].data.length !== 0)
+          setCoords(coordsArr => [...coordsArr, {name: stateInfo.display_name, lat: stateInfo.lat, lon: stateInfo.lon}]);
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const getStateNames = () => {
+    try {
+      const stateArr = [];
+      for (let i = 0; i < states.length; i++){
+        stateArr.push(states[i].name);
+      }
+      setStateNames(stateArr);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  const fetchCovidData = async (stateName) => {
+    try {
+      const response = await axios.get(`https://disease.sh/v3/covid-19/states/${stateName}`);
+      setCovidData(response.data);
+    } 
+    catch (err) {
+      console.error(err);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <MapContainer id="map" center={[38.093498,-98.178923]} zoom={5} scrollWheelZoom={false}>
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+
+      {coords.map((coord, idx) => (
+        <Marker 
+          key={idx} 
+          position={[
+            coord.lat,
+            coord.lon
+          ]}
+          icon={state}
+          click={console.log('hello world')}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Popup>
+            <div>
+              {covidData.state}: 
+              {covidData.cases} cases
+            </div>
+          </Popup>
+        </Marker>
+      ))}
+    </MapContainer>
   );
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import 'leaflet/dist/leaflet.css';
+import 'leaflet-geosearch/dist/geosearch.css';
+import "https://unpkg.com/leaflet-geosearch@3.0.0/dist/geosearch.umd.js";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
